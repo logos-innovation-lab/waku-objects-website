14395cfcf4dfc28fb61c95dfc361fe6e6309aba9
Barbara Gomes Carano <barbeegomes@gmail.com>
2023-09-24 15:16:12 -0400

    fix: update Learn More button icon

---
+++ src/lib/components/icons/information.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { IconProps } from '$lib/types'
+
+	type $$Props = IconProps
+
+	export let size = 20
+	export let title: string | undefined = undefined
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 32 32"
+	width={size}
+	height={size}
+	aria-labelledby={title}
+	role="img"
+>
+	<title>{title}</title>
+	<path
+		d="M17 22L17 14 13 14 13 16 15 16 15 22 12 22 12 24 20 24 20 22 17 22zM16 8a1.5 1.5 0 101.5 1.5A1.5 1.5 0 0016 8z"
+	/><path
+		d="M16,30A14,14,0,1,1,30,16,14,14,0,0,1,16,30ZM16,4A12,12,0,1,0,28,16,12,12,0,0,0,16,4Z"
+	/>
+</svg>
